0e75d025c7d70794a3fb81e6f5b9d92ad1728335
tyty <tyty.jenkins.game@gmail.com>
2023-03-10 23:25:05 +0100

    spawn point

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,7 @@
 // variables
 // -----------------------------
 let btnPlay = document.getElementById('btnPlay');
+let btnStop = document.getElementById('btnStop');
 let gameBox = document.getElementById('gameBox');
 
 
@@ -14,23 +15,31 @@ let getRandowCoord = (min, max) => {
 
 let getPointCoord = () => {
     let boxSize = gameBox.getBoundingClientRect();
-    let boxWidth = getRandowCoord(0, (gameBox.offsetWidth - 15));
-    let boxHeight = getRandowCoord(0, (gameBox.offsetHeight - 15));
+    let boxWidth = getRandowCoord(0, (gameBox.offsetWidth - 30));
+    let boxHeight = getRandowCoord(0, (gameBox.offsetHeight - 30));
     let boxContent = [boxWidth, boxHeight];
 
     return boxContent;
 }
 
-
-let pointSpawn = () => {
-    let boxSize = gameBox.getBoundingClientRect();
-    console.log(gameBox.offsetHeight);
-    let box = getPointCoord();
-    gameBox.innerHTML += '<div class="point" style="top:' + box[1] +'px; left:' + box[0] +'px;"></div>';
+let clearAllPoint = () => {
+    let spawnedPoint = document.getElementsByClassName('point');
+    spawnedPoint.remove();
 }
 
-
+let pointSpawn = () => {
+    let spawn = setInterval(function(){
+        let boxSize = gameBox.getBoundingClientRect();
+        let box = getPointCoord();
+        gameBox.innerHTML += '<div id="' + box[1] + box[0] + '" class="point" style="top:' + box[1] +'px; left:' + box[0] +'px;"></div>';
+      },1000);
+    setTimeout (function() { clearInterval(spawn);}, 3000);
+}
 
 // events
 // -----------------------------
 btnPlay.addEventListener('click', pointSpawn);
+btnStop.addEventListener('click', clearAllPoint);
+
+
+
